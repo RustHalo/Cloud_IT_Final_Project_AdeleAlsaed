8461687e64191aa47aae9a743d262148e63795d3
RustHalo <Adele.Alsaed@Student.HTW-Berlin.de>
2026-08-15 14:34:34 +0200

    link update

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,8 @@ app.add_middleware(
         "http://localhost:8080",
         "http://127.0.0.1:8080",
         "http://localhost:5500",   
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
+        "https://administrative-form-organizer.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
